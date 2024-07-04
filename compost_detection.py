dc6bcb853ef99b83f0e17a697c81e72167b917f8
Leroy <130967155+S10243705-LeroyPeh@users.noreply.github.com>
2024-07-04 13:53:17 +0800

    hello

--- compost_detection.py
+++ compost_detection.py
@@ -65,7 +65,13 @@ def main(_argv):
             tfflie.name = DEMO_VIDEO
     else:
         tfflie.write(video_file_buffer.read())
-        vid = cv2.VideoCapture(tfflie.name)
+        file_type = video_file_buffer.type.split('/')[0]
+        if file_type == 'image':
+            image = Image.open(tfflie.name)
+            image = np.array(image)
+            vid = None
+        else:
+            vid = cv2.VideoCapture(tfflie.name)
 
     class_names = utils.read_class_names(cfg.YOLO.CLASSES)
     custom_classes = st.sidebar.checkbox('Use Custom Classes')
@@ -94,25 +100,111 @@ def main(_argv):
         saved_model_loaded = tf.saved_model.load(FLAGS.weights, tags=[tag_constants.SERVING])
         infer = saved_model_loaded.signatures['serving_default']
 
-    width = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
-    height = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    fps = int(vid.get(cv2.CAP_PROP_FPS))
-    codec = cv2.VideoWriter_fourcc(*'VP80')  # Corrected codec for webm
-    out = cv2.VideoWriter('output.webm', codec, fps, (width, height))
+    if vid:
+        width = int(vid.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        fps = int(vid.get(cv2.CAP_PROP_FPS))
+        codec = cv2.VideoWriter_fourcc(*'VP80')  # Corrected codec for webm
+        out = cv2.VideoWriter('output.webm', codec, fps, (width, height))
 
-    frame_num = 0
+        frame_num = 0
 
-    while True:
-        return_value, frame = vid.read()
-        if not return_value:
-            break
+        while True:
+            return_value, frame = vid.read()
+            if not return_value:
+                break
 
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        frame_num += 1
-        image = Image.fromarray(frame)
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            frame_num += 1
+            image = Image.fromarray(frame)
 
-        frame_size = frame.shape[:2]
-        image_data = cv2.resize(frame, (input_size, input_size))
+            frame_size = frame.shape[:2]
+            image_data = cv2.resize(frame, (input_size, input_size))
+            image_data = image_data / 255.
+            image_data = image_data[np.newaxis, ...].astype(np.float32)
+            start_time = time.time()
+
+            if FLAGS.framework == 'tflite':
+                interpreter.set_tensor(input_details[0]['index'], image_data)
+                interpreter.invoke()
+                pred = [interpreter.get_tensor(output_details[i]['index']) for i in range(len(output_details))]
+                if FLAGS.model == 'yolov3' and FLAGS.tiny:
+                    boxes, pred_conf = filter_boxes(pred[1], pred[0], score_threshold=confidence, input_shape=tf.constant([input_size, input_size]))
+                else:
+                    boxes, pred_conf = filter_boxes(pred[0], pred[1], score_threshold=confidence, input_shape=tf.constant([input_size, input_size]))
+            else:
+                batch_data = tf.constant(image_data)
+                pred_bbox = infer(batch_data)
+                for key, value in pred_bbox.items():
+                    boxes = value[:, :, 0:4]
+                    pred_conf = value[:, :, 4:]
+
+            boxes, scores, classes, valid_detections = tf.image.combined_non_max_suppression(
+                boxes=tf.reshape(boxes, (tf.shape(boxes)[0], -1, 1, 4)),
+                scores=tf.reshape(pred_conf, (tf.shape(pred_conf)[0], -1, tf.shape(pred_conf)[-1])),
+                max_output_size_per_class=50,
+                max_total_size=50,
+                iou_threshold=FLAGS.iou,
+                score_threshold=confidence
+            )
+
+            original_h, original_w, _ = frame.shape
+            bboxes = utils.format_boxes(boxes.numpy()[0], original_h, original_w)
+            pred_bbox = [bboxes, scores.numpy()[0], classes.numpy()[0], valid_detections.numpy()[0]]
+
+            allowed_classes = list(class_names.values())
+            if custom_classes:
+                allowed_classes = assigned_class
+
+            if FLAGS.crop:
+                crop_rate = 150
+                crop_path = os.path.join(os.getcwd(), 'detections', 'crop', 'video_name')
+                try:
+                    os.mkdir(crop_path)
+                except FileExistsError:
+                    pass
+                if frame_num % crop_rate == 0:
+                    final_path = os.path.join(crop_path, 'frame_' + str(frame_num))
+                    try:
+                        os.mkdir(final_path)
+                    except FileExistsError:
+                        pass          
+                    crop_objects(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB), pred_bbox, final_path, allowed_classes)
+
+            if FLAGS.count:
+                counted_classes = count_objects(pred_bbox, by_class=False, allowed_classes=allowed_classes)
+                for key, value in counted_classes.items():
+                    print(f"Number of {key}s: {value}")
+                image = utils.draw_bbox(frame, pred_bbox, FLAGS.info, counted_classes, allowed_classes=allowed_classes, read_plate=FLAGS.plate)
+            else:
+                image = utils.draw_bbox(frame, pred_bbox, FLAGS.info, allowed_classes=allowed_classes, read_plate=FLAGS.plate)
+
+            fps = 1.0 / (time.time() - start_time)
+            print(f"FPS: {fps:.2f}")
+            result = np.asarray(image)
+            result = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+            if save_video:
+                out.write(result)
+
+            result = image_resize(result, width=720)
+            stframe.image(result, channels='BGR', use_column_width=True)
+
+        st.success('Video saved')
+        st.text('Video is Processed')
+        
+        output_vid = open('output.webm', 'rb')
+        out_bytes = output_vid.read()
+        
+        st.text('Output Video')
+        st.video(out_bytes)
+        vid.release()
+        out.release()
+    else:
+        # Handle image
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        frame_size = image.shape[:2]
+        image_data = cv2.resize(image, (input_size, input_size))
         image_data = image_data / 255.
         image_data = image_data[np.newaxis, ...].astype(np.float32)
         start_time = time.time()
@@ -141,7 +233,7 @@ def main(_argv):
             score_threshold=confidence
         )
 
-        original_h, original_w, _ = frame.shape
+        original_h, original_w, _ = image.shape
         bboxes = utils.format_boxes(boxes.numpy()[0], original_h, original_w)
         pred_bbox = [bboxes, scores.numpy()[0], classes.numpy()[0], valid_detections.numpy()[0]]
 
@@ -150,49 +242,28 @@ def main(_argv):
             allowed_classes = assigned_class
 
         if FLAGS.crop:
-            crop_rate = 150
-            crop_path = os.path.join(os.getcwd(), 'detections', 'crop', 'video_name')
+            final_path = os.path.join(os.getcwd(), 'detections', 'crop', 'image')
             try:
-                os.mkdir(crop_path)
+                os.mkdir(final_path)
             except FileExistsError:
                 pass
-            if frame_num % crop_rate == 0:
-                final_path = os.path.join(crop_path, 'frame_' + str(frame_num))
-                try:
-                    os.mkdir(final_path)
-                except FileExistsError:
-                    pass          
-                crop_objects(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB), pred_bbox, final_path, allowed_classes)
+            crop_objects(cv2.cvtColor(image, cv2.COLOR_BGR2RGB), pred_bbox, final_path, allowed_classes)
 
         if FLAGS.count:
             counted_classes = count_objects(pred_bbox, by_class=False, allowed_classes=allowed_classes)
             for key, value in counted_classes.items():
                 print(f"Number of {key}s: {value}")
-            image = utils.draw_bbox(frame, pred_bbox, FLAGS.info, counted_classes, allowed_classes=allowed_classes, read_plate=FLAGS.plate)
+            image = utils.draw_bbox(image, pred_bbox, FLAGS.info, counted_classes, allowed_classes=allowed_classes, read_plate=FLAGS.plate)
         else:
-            image = utils.draw_bbox(frame, pred_bbox, FLAGS.info, allowed_classes=allowed_classes, read_plate=FLAGS.plate)
+            image = utils.draw_bbox(image, pred_bbox, FLAGS.info, allowed_classes=allowed_classes, read_plate=FLAGS.plate)
 
         fps = 1.0 / (time.time() - start_time)
         print(f"FPS: {fps:.2f}")
         result = np.asarray(image)
         result = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
 
-        if save_video:
-            out.write(result)
-
         result = image_resize(result, width=720)
         stframe.image(result, channels='BGR', use_column_width=True)
-
-    st.success('Video saved')
-    st.text('Video is Processed')
-    
-    output_vid = open('output.webm', 'rb')
-    out_bytes = output_vid.read()
-    
-    st.text('Output Video')
-    st.video(out_bytes)
-    vid.release()
-    out.release()
 
 if __name__ == '__main__':
     try:
